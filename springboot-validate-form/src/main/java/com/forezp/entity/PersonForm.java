af82a899e4697b44772c3ebfd2d3e7651a6d1b28
Forezp <124746406@qq.com>
2017-04-30 16:53:03 +0800

    第十九篇： 验证表单信息

---
+++ springboot-validate-form/src/main/java/com/forezp/entity/PersonForm.java
@@ -0,0 +1,38 @@
+package com.forezp.entity;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+/**
+ * Created by fangzhipeng on 2017/4/19.
+ */
+public class PersonForm {
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String name;
+
+    @NotNull
+    @Min(18)
+    private Integer age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Person(Name: " + this.name + ", Age: " + this.age + ")";
+    }
+}
---
+++ springboot-validate-form/src/main/java/com/forezp/web/WebController.java
@@ -0,0 +1,38 @@
+package com.forezp.web;
+
+import com.forezp.entity.PersonForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.validation.Valid;
+
+/**
+ * Created by fangzhipeng on 2017/4/19.
+ */
+@Controller
+public class WebController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm) {
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
